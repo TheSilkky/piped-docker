a544d1403f8cc2ee333253d64e90f73b5f9c1bfb
Ellie <me@silkky.dev>
2022-10-01 15:57:31 +1000

    Fix

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-sed -i -e "s|pipedapi.kavin.rocks|$API_URL|g" /srv/assets/*
+export DEFAULT_API_URL=api.piped.silkky.dev
+export API_URL="${API_URL:-${DEFAULT_API_URL}}"
+
+sed -i "s/pipedapi.kavin.rocks/$API_URL/g" /srv/assets/*
 
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
