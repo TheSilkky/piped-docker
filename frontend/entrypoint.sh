0f6248b39ceb2ae10fcc4b043dea7533e316c665
Ellie <me@silkky.dev>
2023-07-08 13:10:36 +1000

    Use git submodules to get source

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -5,13 +5,4 @@ set -ex
 api="${API_HOST}"
 sed -i "s/pipedapi.kavin.rocks/$api/g" /usr/share/nginx/html/assets/*
 
-# Localize fonts
-base="https://fonts\.(gstatic\.com|kavin\.rocks)"
-for font in $fonts; do
-    file="/usr/share/nginx/html/fonts$(echo "$font" | sed -E "s#$base##")"
-    mkdir -p "$(dirname "$file")"
-    curl -L "$font" -o "$file"
-done
-sed -Ei "s#$base#/fonts#g" /usr/share/nginx/html/assets/*
-
 exec "$@"
